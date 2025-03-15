4b3d4258fb2b64676496359e86ccdc78c66ddff0
Mahdi Hasan Tazelly <mahdihasantazelly@gmail.com>
2025-03-15 11:24:34 +0600

    Implemented DELETE: /doctors/:id, PUT: /doctors/:id, PUT: /hospitals/:id, DELETE: /hospitals/:id

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 import {DoctorsModel} from './models/DoctorsModel.js';
 import { HospitalsModel } from './models/HospitalsModel.js';
 import {AdminModel} from './models/AdminModel.js';
 
 import { authenticate, generateToken } from './utils/middleware.js';
-import { error } from 'console';
 
 
 const app = express();
@@ -26,6 +26,7 @@ app.listen(PORT,
 app.use(express.json());
 //helps to parse url-encoded form data
 app.use( express.urlencoded({extended: true}) );
+app.use(cookieParser());
 
 // for testing purpose
 app.get("/test", (req, res)=>{
@@ -44,11 +45,35 @@ app.get("/admin/login",
                 admin.save();
                 return res.cookie('sessiontoken',loginToken).json({message: "Login Successfull!"});
             }else{
-                return res.json({error: "Invalid credentials!"});
+                return res.status(400).json({error: "Invalid credentials!"});
             }
         }catch(error){
             console.log(error);
         }
+});
+
+
+app.get("/admin/logout", async(req, res)=>{
+    try{
+        const sessiontoken = req.cookies.sessiontoken;
+        const admin = await AdminModel.findOne({sessiontoken: `${sessiontoken}`});
+        const newAdmin = await AdminModel({
+            username: admin.username,
+            email: admin.email,
+            password: admin.password
+        });
+
+        await AdminModel.deleteOne({email: `${admin.email}`});
+
+        newAdmin.save()
+        .then( ()=>{
+            res.json({message: "Logged out successfully!"});
+        })
+        
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: "Internal server error!"});
+    }
 })
 
 app.get("/hospitals", async(req, res)=>{
@@ -67,7 +92,7 @@ app.get("/hospitals/:id", async(req, res)=>{
 
         let hospital = await HospitalsModel.findById(id);
         if( !hospital){
-            return res.json({error: "No hospital found!"})
+            return res.status(400).json({error: "No hospital found!"});
         }
         res.json(hospital);
     }catch(err){
@@ -87,18 +112,51 @@ app.post("/hospitals", async(req, res)=>{
             !hospital.location.subDistrict ||
             !hospital.location.holdingNo 
         ){
-            return res.json({error: "Fill all the informaitions."});
+            return res.status(400).json({error: "Fill all the informaitions."});
         }
         const newHospital = await HospitalsModel(hospital);
-        newHospital.save()
-        .then( ()=>{
-            return res.json("Hospital is added.");
-        });
-        //res.json(hospital);
-    }catch(err){
-        console.log(err);
+        await newHospital.save()
+        
+        res.json({message: "Hospital is added."});
+        
+    }catch(error){
+        console.log(error);
+        if(error.code === 11000){
+            return res.json({message: "Registration number should be unique."}).status(400);
+        }
+        res.status(500).json({message: "Internal server error."});
     }
 })
+
+app.delete("/hospitals/:id", async(req, res)=>{
+    try{
+        let {id} = req.params;
+        const deletedHospital = await HospitalsModel.findByIdAndDelete(id);
+        if( deletedHospital ){
+            return res.json({message: "Hospital is deleted."});
+        }
+        res.json({message: "Cannot be deleted!"}).status(400);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: "Internal server Error."});
+    }
+});
+
+app.put("/hospitals/:id", async(req, res)=>{
+    try{
+        let {id} = req.params;
+        let updated = req.body;
+        const hospital = await HospitalsModel.findByIdAndUpdate(id, {...updated});
+        if (hospital){
+            console.log(updated);
+            return res.json({message: "Information is updated."});
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: "Internal server error."});
+    }
+
+});
 
 // ---------------------DOCTORS ROUTES---------------------
 
@@ -112,7 +170,7 @@ app.get("/doctors/:id", async(req, res)=>{
         let {id} = req.params;
         // if dosen't math the mongodb id format
         if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-            return res.json({ error: "Invalid doctor ID format." });
+            return res.json({ error: "Invalid doctor ID format." }).status(400);
         }
 
         let doctor = await DoctorsModel.findById(id);
@@ -123,7 +181,7 @@ app.get("/doctors/:id", async(req, res)=>{
 
         res.json(doctor);
     }catch(err){
-        console.log(err);
+        res.status(400).json({message: "Internal Server error."});
     }
 })
 
@@ -136,16 +194,50 @@ app.post("/doctors", async(req, res)=>{
             !doctor.degree ||
             !doctor.specialization ||
             !doctor.contactNo
-    ){
-        return res.json({error: "Fill all informations."})
-    }
-    const newDoctor = await DoctorsModel(doctor);
-    newDoctor.save()
-    .then( ()=>{
-        console.log("Doctors information is added.")
-    });
-    //res.json(doctor);
+        ){
+            return res.status(400).json({error: "Fill all informations."});
+        }
+        const newDoctor = await DoctorsModel(doctor);
+        await newDoctor.save()
+        res.json({message: "Doctors information is saved."});
     }catch(error){
-        console.log(error);
+        if(error.code === 11000 ){
+            return res.status(400).json({message: "Registration number should be unique."});
+        }
+        res.status(500).json({message: "Internal Server Error."});
     }
-})
+});
+
+app.put("/doctors/:id", async(req, res)=>{
+    try{
+        let {id} = req.params;
+        let update = req.body;
+        const doctor = await DoctorsModel.findById(id);
+        if(!doctor){
+            return res.status(400).json({message: "No such doctor Found."});
+        }
+        
+        const updatedDoc = await DoctorsModel.findByIdAndUpdate(id, {...update});
+        console.log(updatedDoc);
+        res.json({message: "Information is updated."});
+    }catch(error){
+        res.status(500).json({message: "Internal server error."});
+    }
+
+});
+
+app.delete("/doctors/:id", async(req, res)=>{
+    try{
+        let {id} = req.params;
+        const doctor = await DoctorsModel.findById(id);
+        if( !doctor ){
+            return res.status(400).json({message: "No doctor found."});
+        }
+        await DoctorsModel.findByIdAndDelete(id);
+        console.log(doctor);
+        res.json({message: "Doctor is deleted."});
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: "Internal server error."})
+    }
+});
--- models/AdminModel.js
+++ models/AdminModel.js
@@ -8,7 +8,8 @@ export const AdminSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     password: {
         type: String,
